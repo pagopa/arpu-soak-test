fb48b5faaa54af97b22622f6e291d0c5947f84c2
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2026-03-17 12:23:00 +0100

    updated broker api

--- src/api/broker.js
+++ src/api/broker.js
@@ -5,15 +5,15 @@ import { buildDefaultParams } from "../common/envVars";
 
 export const BROKER_API_NAMES = {
     getPublicBrokerInfo: "broker/getPublicBrokerInfo"
-}
+};
 
-const BASE_URL = getBaseUrl();
+const baseUrl = getBaseUrl();
 
 export function getPublicBrokerInfo(brokerId, externalId) {
     const apiName = BROKER_API_NAMES.getPublicBrokerInfo;
     const params = buildDefaultParams(apiName);
 
-    const res = http.get(`${BASE_URL}/public/brokers?brokerId=${brokerId}&externalId=${externalId}`, params);
+    const res = http.get(`${baseUrl}/public/brokers?brokerId=${brokerId}&externalId=${externalId}`, params);
 
     logResult(apiName, res);
     return res;
