fd22ff398446b2519ca454667fc17d41c62e0222
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2026-03-26 17:47:38 +0100

    feat: P4PU-1211 create soak test Installment (#6)

---
+++ src/tests/installment/getDebtorUnpaidDebtPositionInstallments.js
@@ -0,0 +1,63 @@
+import { assert, statusOk } from "../../common/assertions.js";
+import {
+  getDebtorUnpaidDebtPositionInstallments,
+  INSTALLMENT_API_NAMES
+} from "../../api/installment.js";
+import {
+  getPagedUnpaidDebtPositions
+} from "../../api/debtPosition.js";
+import defaultHandleSummaryBuilder from "../../common/handleSummaryBuilder.js";
+import { defaultApiOptionsBuilder } from "../../common/dynamicScenarios/defaultOptions.js";
+import { logErrorResult } from "../../common/dynamicScenarios/utils.js";
+import { getAuthToken, getTestEntity, abort, getAuthFiscalCode } from "../../common/utils.js";
+import { CONFIG } from "../../common/envVars.js";
+
+const application = "installment";
+const testName = "getDebtorUnpaidDebtPositionInstallments";
+
+// Dynamic scenarios' K6 configuration
+export const options = defaultApiOptionsBuilder(
+  application,
+  testName,
+  [
+    INSTALLMENT_API_NAMES.getDebtorUnpaidDebtPositionInstallments
+  ] // applying apiName tags to thresholds
+);
+
+// K6 summary configuration
+export const handleSummary = defaultHandleSummaryBuilder(application, testName);
+
+export function setup() {
+   const authToken = getAuthToken();
+   const xFiscalCode = getAuthFiscalCode(authToken);
+   const brokerId = CONFIG.CONTEXT.BROKER_ID;
+
+   const debtPositions = getPagedUnpaidDebtPositions(brokerId, xFiscalCode, authToken).json().content;
+
+   if (debtPositions.length === 0) {
+        abort("No elements found in debtPositions list please restart test with at least one element");
+   }
+
+  return {
+    brokerId,
+    debtPositions,
+    token: authToken
+  };
+}
+
+export default (data) => {
+  const debtPosition = getTestEntity(data.debtPositions);
+  const paymentOption = getTestEntity(debtPosition.paymentOptions);
+
+  const getDebtorUnpaidDebtPositionInstallmentsResult = getDebtorUnpaidDebtPositionInstallments(data.brokerId, debtPosition.debtPositionId, paymentOption.paymentOptionId, debtPosition.organizationId, data.token);
+
+  assert(getDebtorUnpaidDebtPositionInstallmentsResult, [statusOk()]);
+
+  if (getDebtorUnpaidDebtPositionInstallmentsResult.status !== 200) {
+    logErrorResult(
+        `Unexpected ${testName} status`, 
+        getDebtorUnpaidDebtPositionInstallmentsResult, 
+        true
+    );
+  }
+};
